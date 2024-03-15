2d57ece48cf48e660d256a220c5cac6539533c38
necati <necati.arslan2018@gmail.com>
2024-03-15 11:35:02 +0100

    solved

--- src/com/neco/Main.java
+++ src/com/neco/Main.java
@@ -4,25 +4,45 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int n = in.nextInt();
+      /*ava has 8 primitive data types; char, boolean, byte, short, int, long, float, and double. For this exercise, we'll work with the primitives used to hold integer values (byte, short, int, and long):
 
-            String result="";
-            int bTotal =0;
-            for(int j=0; j<n; j++ ) {
-                bTotal += (int)Math.pow(2, j)*b;
-                int total=a+ bTotal;
-                result += total +" ";
+A byte is an 8-bit signed integer.
+A short is a 16-bit signed integer.
+An int is a 32-bit signed integer.
+A long is a 64-bit signed integer.*/
+
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+
+            try
+            {
+                long n=sc.nextLong();
+                System.out.println(n+" can be fitted in:");
+                if (n >= -128 && n <= 127)
+                    System.out.println("* byte");
+                if (n >= -32768 && n <= 32767)
+                    System.out.println("* short");
+                if (n >= -2147483648 && n <= 2147483647)
+                    System.out.println("* int");
+                if (n >= Long.MIN_VALUE && n <= Long.MAX_VALUE)
+                    System.out.println("* long");
+                //Complete the code
             }
-            System.out.println(result);
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
 
         }
-        in.close();
 
-    }
+
+
+
+
+
+    }//main
 }
 //https://www.hackerrank.com/challenges/java-loops/problem?isFullScreen=true
