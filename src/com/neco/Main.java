2923ed4b3ff1bb3d18927363175bc4061f881a66
necati <necati.arslan2018@gmail.com>
2024-03-15 12:24:19 +0100

    solved

--- src/com/neco/Main.java
+++ src/com/neco/Main.java
@@ -2,18 +2,27 @@ package com.neco;
 import java.util.Scanner;
 
 public class Main {
+    // Static Initialization Blocks
+    static int B,H;
+    static boolean flag;
+
+    static{
+        Scanner scan=new Scanner(System.in);
+        B=scan.nextInt();
+        H=scan.nextInt();
+        if(B>0&&H>0){
+            flag=true;
+        }else{
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
 
     public static void main(String[] args) {
 
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner scanner = new Scanner(System.in);
-
-        int lineNumber = 1; //
-
-        while (scanner.hasNext()) {
-            String line = scanner.nextLine();
-            System.out.println(lineNumber + " " + line);
-            lineNumber++; //
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
         }
 
 
@@ -21,4 +30,24 @@ public class Main {
 
     }//main
 }
-//https://www.hackerrank.com/challenges/java-loops/problem?isFullScreen=true
+
+/*
+* public class MyClass {
+    private static int number;
+
+    static {
+        init(); // Statik başlatma bloğu içinde metodu çağırıyoruz
+    }
+
+    private static void init() {
+        // Static initialization method
+        System.out.println("Static initialization method is executed.");
+        number = 10;
+    }
+
+    public static void main(String[] args) {
+        // Accessing static variable after static initialization method
+        System.out.println("Number: " + number);
+    }
+}
+* */
