b9218d00d20c1a0f49a51125689202246ed1ce7b
necati <necati.arslan2018@gmail.com>
2024-03-18 22:15:03 +0100

    solved

--- src/com/neco/Main.java
+++ src/com/neco/Main.java
@@ -1,26 +1,60 @@
 package com.neco;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
+    public static String data="1 1 1 0 0 0\n" +
+            "0 1 0 0 0 0\n" +
+            "1 1 1 0 0 0\n" +
+            "0 0 2 4 4 0\n" +
+            "0 0 0 2 0 0\n" +
+            "0 0 1 2 4 0";
 
+
+    // Complete the hourglassSum function below.
+    static int hourglassSum(List<List<Integer>> arr) {
+        int max_hourglass = -63; //
+
+        for (int i = 0; i < arr.size() - 2; i++) {
+            for (int j = 2; j < arr.get(0).size(); j++) {
+                int k = j - 2, hourglass = 0;
+                while (k <= j) {
+                    hourglass += arr.get(i).get(k) + arr.get(i + 2).get(k);
+                    ++k;
+                }
+                hourglass += arr.get(i + 1).get(j - 1); // Middle value
+                max_hourglass = Math.max(hourglass, max_hourglass);
+            }
+        }
+        return max_hourglass;
+    }
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int[] a =new int[n];
 
-        for (int j=0; j<n;j++){
-            int v= scan.nextInt();
-            a[j]=v;
-        }
-        scan.close();
+        List<List<Integer>> arr = new ArrayList<>();
 
-        // Prints each sequential element in array a
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+
+        String[] rows = data.split("\n");
+
+
+        for (String row : rows) {
+            String[] elements = row.split(" ");
+
+            List<Integer> arrRowItems = new ArrayList<>();
+            for (String element : elements) {
+                int arrItem = Integer.parseInt(element);
+                arrRowItems.add(arrItem);
+            }
+
+            arr.add(arrRowItems);
         }
+        System.out.println(hourglassSum(arr));
+
+
 
 
 
