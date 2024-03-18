4e5ba0714d518bed4fdeece444fc5a425b82380b
necati <necati.arslan2018@gmail.com>
2024-03-18 11:26:38 +0100

    solved

--- src/com/neco/Main.java
+++ src/com/neco/Main.java
@@ -2,29 +2,25 @@ package com.neco;
 import java.util.Scanner;
 
 public class Main {
-    // Static Initialization Blocks
-    static int B,H;
-    static boolean flag;
 
-    static{
-        Scanner scan=new Scanner(System.in);
-        B=scan.nextInt();
-        H=scan.nextInt();
-        if(B>0&&H>0){
-            flag=true;
-        }else{
-            System.out.println("java.lang.Exception: Breadth and height must be positive");
-        }
-    }
 
 
     public static void main(String[] args) {
 
-        if(flag){
-            int area=B*H;
-            System.out.print(area);
-        }
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a =new int[n];
 
+        for (int j=0; j<n;j++){
+            int v= scan.nextInt();
+            a[j]=v;
+        }
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
 
 
 
