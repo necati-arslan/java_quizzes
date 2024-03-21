850514e87372375dc45572a1318961dce0ba0c7c
necati <necati.arslan2018@gmail.com>
2024-03-21 22:26:03 +0100

    solved

--- src/com/neco/Main.java
+++ src/com/neco/Main.java
@@ -6,53 +6,35 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static String data="1 1 1 0 0 0\n" +
-            "0 1 0 0 0 0\n" +
-            "1 1 1 0 0 0\n" +
-            "0 0 2 4 4 0\n" +
-            "0 0 0 2 0 0\n" +
-            "0 0 1 2 4 0";
-
-
-    // Complete the hourglassSum function below.
-    static int hourglassSum(List<List<Integer>> arr) {
-        int max_hourglass = -63; //
-
-        for (int i = 0; i < arr.size() - 2; i++) {
-            for (int j = 2; j < arr.get(0).size(); j++) {
-                int k = j - 2, hourglass = 0;
-                while (k <= j) {
-                    hourglass += arr.get(i).get(k) + arr.get(i + 2).get(k);
-                    ++k;
-                }
-                hourglass += arr.get(i + 1).get(j - 1); // Middle value
-                max_hourglass = Math.max(hourglass, max_hourglass);
-            }
-        }
-        return max_hourglass;
-    }
 
     public static void main(String[] args) {
 
 
-        List<List<Integer>> arr = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+        List<List<Integer>> lines = new ArrayList<List<Integer>>();
+        int n = in.nextInt();
+        for(int i=0; i<n ;i++){
+            List<Integer> line = new ArrayList<Integer>();
+            int d = in.nextInt();
+            for(int j=0; j<d;j++){
+                line.add(in.nextInt());
+            }
+            lines.add(line);
+        }
+        int q = in.nextInt();
+        for (int i = 0; i < q; i++) {
 
-
-        String[] rows = data.split("\n");
-
-
-        for (String row : rows) {
-            String[] elements = row.split(" ");
-
-            List<Integer> arrRowItems = new ArrayList<>();
-            for (String element : elements) {
-                int arrItem = Integer.parseInt(element);
-                arrRowItems.add(arrItem);
+            int x = in.nextInt();
+            int y = in.nextInt();
+            if (y > lines.get(x - 1).size()) {
+                System.out.println("ERROR!");
+            } else {
+                System.out.println(lines.get(x - 1).get(y - 1));
             }
 
-            arr.add(arrRowItems);
         }
-        System.out.println(hourglassSum(arr));
+        in.close();
+
 
 
 
